16791961d360c6c33ee21acf542d663d35fba89d
SerGobec <48455848+SerGobec@users.noreply.github.com>
2022-12-24 00:00:30 +0200

    Solved

---
+++ CW_Solutions/Find the unique string/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Find_the_unique_string
+{
+    public class Kata
+    {
+        public static int GetUnique(IEnumerable<int> numbers)
+        {
+            return numbers.GroupBy(x => x).First(el => el.Count() == 1).Key;
+        }
+    }
+}
