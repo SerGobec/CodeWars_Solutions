36302cbb5a8ba5d24e9a0cc5da71889db2a37db0
SerGobec <48455848+SerGobec@users.noreply.github.com>
2022-12-24 04:15:32 +0200

    Solved kata

---
+++ CW_Solutions/RectangleRotationLibrary/Kata.cs
@@ -0,0 +1,15 @@
+﻿namespace RectangleRotationLibrary
+{
+    public class Kata
+    {
+        public int RectangleRotation(int a, int b)
+        {
+            double half_a = (double)a / 2;
+            double half_b = (double)b / 2;
+            double sqrttwo = Math.Sqrt(2);
+            int sum = ((int)(half_b / sqrttwo) * 2 + 1) * ((int)(half_a / sqrttwo) * 2 + 1);
+            sum += ((int)((half_b - 0.5 * sqrttwo) / sqrttwo + 1) * 2) * ((int)((half_a - 0.5 * sqrttwo) / sqrttwo + 1) * 2);
+            return sum;
+        }
+    }
+}
