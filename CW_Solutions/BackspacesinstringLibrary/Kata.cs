776981feb584e9aa8ff5d48c6c0d43f402a4f27d
SerGobec <48455848+SerGobec@users.noreply.github.com>
2022-12-30 23:30:10 +0200

    Solved

---
+++ CW_Solutions/BackspacesinstringLibrary/Kata.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace BackspacesinstringLibrary
+{
+    // https://www.codewars.com/kata/5727bb0fe81185ae62000ae3/csharp
+    public class Kata
+    {
+        public static string CleanString(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (c == '#')
+                {
+                    if (sb.Length > 0) sb.Length = sb.Length - 1;
+                    continue;
+                }
+                sb.Append(c);
+            }
+            return sb.ToString();
+        }
+    }
+}
